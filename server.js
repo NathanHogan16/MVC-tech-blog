9a38f18d6e65d0288c016a352842177b94025716
Nate Hogan <natek.hogan@gmail.com>
2021-04-26 16:41:39 -0600

    edited code

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.set('view engine', 'handlebars');
 
 const PORT = process.env.PORT || 3001;
 
+app.set("port", PORT);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
